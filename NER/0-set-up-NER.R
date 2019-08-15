77546a24cdab354f35a711217d50aa89dcc74990
kathrinwy <kathrin.wy@hotmail.de>
2019-08-15 09:12:58 -0400

    Adapt to work from different computer

--- NER/0-set-up-NER.R
+++ NER/0-set-up-NER.R
@@ -10,18 +10,20 @@
 
 # Install and set-up ------------------------------------------------------
 
-rm(list = ls())        ## Clear variables etc from cache
-options(scipen = 999)  ## disable scientific notation in R
-## Set username/user-machine 
-# mylaptop.name <- "weny"
-my.username <- "kathrin" 
+rm(list = ls())                          # Clear variables etc from cache
+options(scipen = 999)                    # Disable scientific notation in R
 
+# At work
 NER.directory <- "G:/My Drive/2019/3- Humanitarian data/COD-PS/NER"
 NER.input     <- "G:/My Drive/2019/3- Humanitarian data/COD-PS/NER/input/"
 NER.output    <- "G:/My Drive/2019/3- Humanitarian data/COD-PS/NER/output/"
-setwd(NER.directory)
 
-test test t est
+# At home
+NER.directory <- "C:/Users/kathrinweny/COD-PS/NER"
+NER.input     <- "C:/Users/kathrinweny/COD-PS/NER/input/"
+NER.output    <- "C:/Users/kathrinweny/COD-PS/NER/output/"
+
+setwd(NER.directory)
 
 # Install/load packages ---------------------------------------------------
 
@@ -30,28 +32,29 @@ test test t est
 ## using the DDI codebook. It also makes it easy to apply value labels 
 ## and use other metadata provided by IPUMS, including the geographic boundary files.
 ## https://github.com/mnpopcenter/ipumsr#ipumsr
-library(ipumsr)   ##Package for Census Microsamples
+
+library(ipumsr)                         # Package for Census Microsamples
 library(ggplot2)
 library(dplyr)
-library(foreign)  ##Package for handling proprietary data formats
+library(foreign)                        # Package for handling proprietary data formats
 library(devtools)
 library(readxl)
 library(tidyverse)
 
-##install_github("timriffe/Pyramid",subdir="Pyramid")
+# install_github("timriffe/Pyramid",subdir="Pyramid")
 library(Pyramid)
 
 # install_github( "ajdamico/lodown" , dependencies = TRUE )
-library(lodown)   ##Package to import DHS data directly from WWW
-library(rdhs)     ##Package to handle Demographic Health Surveys
+library(lodown)                          # Package to import DHS data directly from WWW
+library(rdhs)                            # Package to handle Demographic Health Surveys
 
 # install.packages("remotes") -------------------- Step 1 of 2 to install DemoTools
 # remotes::install_github("timriffe/DemoTools") -- Step 2 of 2 to install DemoTools
 
-library(DemoTools)##Package for demog data quality assessment tools 
-library(DDM)      ##Package for DDM, with CDMLTs
-library(childhoodmortality)  ### Child Mortality Package
-library(DHS.rates) ##Package for calculation of DHS rates
+library(DemoTools)                       # Package for demog data quality assessment tools 
+library(DDM)                             # Package for DDM, with CDMLTs
+library(childhoodmortality)              # Child Mortality Package
+library(DHS.rates)                       # Package for calculation of DHS rates
 
 library(bayesTFR)
 library(bayesLife)
